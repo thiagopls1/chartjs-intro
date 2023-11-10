71015b22d55bae417b6b3cfaee16fe11a0fc408a
thiagopls1 <thiagopls1@hotmail.com>
2023-11-10 14:26:44 -0300

    added new column

--- src/script.js
+++ src/script.js
@@ -3,12 +3,19 @@ const ctx = document.getElementById('myChart');
 new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
-    }]
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'],
+    datasets: [
+      {
+        label: '# of Votes for X president',
+        data: [12, 19, 3, 5, 2, 3, 1000],
+        borderWidth: 1
+      },
+      {
+        label: '# of Votes for Y president',
+        data: [10, 23, 4, 1, 9, 13, 1200],
+        borderWidth: 1
+      }
+  ]
   },
   options: {
     scales: {
